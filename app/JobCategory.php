3ca8e97f4d96bbe2c51ed1016741de7382c25dbb
segitiga <rafi.nizar15@gmail.com>
2020-04-14 22:03:15 +0700

    eloquent relation

--- app/JobCategory.php
+++ app/JobCategory.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class JobCategory extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'description',
+    ];
+
+    // one to many jobs
+    public function jobs()
+    {
+        return $this->hasMany('App\Job');
+    }
 }
