fa82045ae1e461c7314575b824921f3985b036a4
segitiga <rafi.nizar15@gmail.com>
2020-04-15 12:46:27 +0700

    dashboard adminlte

---
+++ resources/views/dashboard/layout.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    @include('dashboard.partials._head')
+
+</head>
+
+<body class="hold-transition sidebar-mini">
+
+<div class="wrapper">
+
+    {{-- navbar --}}
+    @include('dashboard.navbars._navbar')
+
+    {{-- sidebar --}}
+    @include('dashboard.navbars._sidebar')
+
+    {{-- content --}}
+    <div class="content-wrapper">
+        @yield('content')
+    </div>
+
+    {{-- sidebar control --}}
+
+    {{-- footer --}}
+    @include('dashboard.footers._footer')
+
+</div>
+
+    {{-- dashboard's js's --}}
+    @include('dashboard.partials._javascripts')
+
+    {{-- own's page js's --}}
+    @yield('scripts')
+
+</body>
+
+</html>
