7ea8b9331e184198edfd4608e28431840c5f94ae
segitiga <rafi.nizar15@gmail.com>
2020-04-15 12:52:43 +0700

    dashboard fix js

--- resources/views/dashboard/partials/_javascripts.blade.php
+++ resources/views/dashboard/partials/_javascripts.blade.php
@@ -1,6 +1,6 @@
     <!-- jQuery -->
-    <script src="{{ asset('dashboard_resources') }}plugins/jquery/jquery.min.js"></script>
+    <script src="{{ asset('dashboard_resources') }}/plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap 4 -->
-    <script src="{{ asset('dashboard_resources') }}plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('dashboard_resources') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
     <script src="{{ asset('dashboard_resources') }}/dist/js/adminlte.min.js"></script>
