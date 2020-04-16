ab5e5879432dbc01bf3252893a2a517501c9914a
segitiga <rafi.nizar15@gmail.com>
2020-04-17 00:39:54 +0700

    admin middleware

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,22 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Student;
+use Session;
 
 class DashboardController extends Controller
 {
     public function getHome(Request $request) {
 
         if ($request->session()->get('role') == 'admin') {
-
             return view('dashboard.pages.admins.home');
-
         } elseif ($request->session()->get('role') == 'employer') {
-            
             return view('dashboard.pages.employers.home');
-
         }
-
         return view('/');
-        
     }
+
+    public function getUserList() {
+
+        $students = Student::paginate(20);
+
+        return view ('dashboard.pages.admins.userlist')->with('students', $students);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// general auth
+Route::post('logout', function () {
+    Session::flush();
+    return redirect('/');
+})->name('logout');
 
+// portal
 Route::get('/', 'PageController@getHome');
 
 // dashboard
-Route::group(['middleware' => 'LoginCheck'], function(){
-    Route::get('dashboard', 'DashboardController@getHome');
-});
+Route::get('dashboard', 'DashboardController@getHome')->middleware('LoginCheck');
 
 // admin
 Route::get('adminsuperscretloginY', 'AdminController@showLogin')->name('admin.showlogin');
@@ -27,5 +31,18 @@ Route::post('adminsuperscretloginY', 'AdminController@Login')->name('admin.login
 
 Route::get('adminsuperscretregisterY', 'AdminController@showRegister')->name('admin.showregister');
 Route::post('adminsuperscretregisterY', 'AdminController@Register')->name('admin.register');
+
+Route::group(['middleware' => 'LoginCheck', 'AdminCheck'], function(){
+    Route::get('admin/user-list', 'DashboardController@getUserList');
+    Route::get('admin/new-employers', 'DashboardController@getNewEmployers');
+    Route::get('admin/approved-employers', 'DashboardController@getAprrovedEmployers');
+    Route::get('admin/unapproved-employers', 'DashboardController@getUnapprovedEmployers');
+    Route::get('admin/new-jobs', 'DashboardController@getNewJobs');
+    Route::get('admin/approved-jobs', 'DashboardController@getApprovedJobs');
+    Route::get('admin/unapproved-jobs', 'DashboardController@getUnapprovedJobs');
+    Route::get('admin/new-seminars', 'DashboardController@getNewSeminars');
+    Route::get('admin/approved-seminars', 'DashboardController@getApprovedSeminars');
+    Route::get('admin/unapproved-seminars', 'DashboardController@getUnapprovedSeminars');
+});
 
 
