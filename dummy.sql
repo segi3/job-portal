1a672cee6d91ed3f5ffa35df35baec4844727531
segitiga <rafi.nizar15@gmail.com>
2020-04-30 16:48:53 +0700

    db

---
+++ dummy.sql
@@ -0,0 +1,9 @@
+insert into employers values ('1', null, '2', 'dosen', 'www.google.com', 'alamat', 'kota', 'provinsi', '1111111111', 'rafi', '081111111111', 'rafi@example.com', 'password', null, null);
+insert into employers values ('2', null, '1', 'dosen', 'www.google.com', 'alamat', 'kota', 'provinsi', '1111111111', 'rafi', '081111111111', 'dodo@example.com', 'password', null, null);
+insert into employers values ('3', null, '0', 'dosen', 'www.google.com', 'alamat', 'kota', 'provinsi', '1111111111', 'rafi', '081111111111', 'aodo@example.com', 'password', null, null);
+
+insert into job_categories values ('1', 'example_category_1', 'deskripsi_cat', null, null);
+
+insert into jobs values ('1', '1', '2', 'coding', 'posisi', '1', 'ngoding', 'freelance', 'keputih', null, 'bisa ngoding', 'bahasa html', 'javascripts', '500000', null, null);
+insert into jobs values ('2', '1', '1', 'Masak', 'posisi', '1', 'Masak', 'freelance', 'Gebang', null, 'bisa masak', 'ngerti sayur', 'ngerti bumbu', '500000', null, null);
+insert into jobs values ('3', '1', '0', 'makan', 'posisi', '1', 'makan nasi goreng', 'freelance', 'keputih', null, 'bisa makan banyak', 'tau cara make pisau', 'sering makan', '500000', null, null);
