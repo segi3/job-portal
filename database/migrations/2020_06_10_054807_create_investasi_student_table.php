4d27b84129abba133dba448c73c388992707528d
segitiga <rafi.nizar15@gmail.com>
2020-06-13 18:36:32 +0700

    update kolom investasi_student

--- database/migrations/2020_06_10_054807_create_investasi_student_table.php
+++ database/migrations/2020_06_10_054807_create_investasi_student_table.php
@@ -22,6 +22,7 @@ class CreateInvestasiStudentTable extends Migration
             $table->integer('lembar_beli');
             $table->string('berkas_ktp');
             $table->string('berkas_bukti_pembayaran');
+            $table->date('expired_at');
 
             $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('investasi_id')->references('id')->on('investasi')->onDelete('cascade')->onUpdate('cascade');
