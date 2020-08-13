c0e92550a53328eaedaa98f98991d8ea659f01cb
iqbaaaaal <57170373+IqbaalPratama20@users.noreply.github.com>
2020-08-13 20:13:53 +0700

    footer+db iyt

--- app/Investasi_IYT.php
+++ app/Investasi_IYT.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Investasi_IYT extends Model
 {
     protected $fillable = [
-       'nama_investasi', 'investee_id', 'status', 'bank', 'no_rekening', 'atas_nama',
-       'deskripsi_bisnis', 'roi_top', 'roi_bot', 'tgl_jatuh_tempo', 'berkas_porposal_investasi',
-       'berkas_laporan_keuangan', 'admin_id',
+       'nama_kelompok', 'status','investee_id', 'nama_ketua','berkas_pitch_desk','berkas_proposal_bisnis', 'admin_id',
     ];
 }
